4e19caf6148ce9c3fbca2ad48c9cfd569fc9a77c
vitorkravs <vitorkravs3@gmail.com>
2023-04-14 19:35:23 -0300

    projeto_completo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import { QuizContext } from './context/quiz'
 
 import Welcome from './components/Welcome'
 import Question from './components/Question'
+import GameOver from './components/GameOver'
 
 import './App.css'
 
@@ -18,6 +19,7 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === 'Start' && <Welcome/>}
       {quizState.gameStage === 'Playing' && <Question/>}
+      {quizState.gameStage === 'End' && <GameOver/>}
     </div>
   )
 }
--- vite-project/src/context/quiz.jsx
+++ vite-project/src/context/quiz.jsx
@@ -8,6 +8,8 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestion: 0,
+    score: 0,
+    answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
@@ -27,10 +29,32 @@ const quizReducer = (state, action) => {
             }
         case "CHANGE_QUESTION":
             const nextQuestion = state.currentQuestion +1
+            let endGame = false
+
+            if(!questions[nextQuestion]){
+                endGame = true
+            }
+
             return{
                 ...state,
                 currentQuestion: nextQuestion,
+                gameStage: endGame ? STAGES[2] : state.gameStage,
+                answerSelected: false
             }
+        case "NEW_GAME":
+            return initialState
+        case "CHECK_ANSWER":
+                if(state.answerSelected) return state
+                const answer = action.payload.answer
+                const option = action.payload.option
+                let correctAnswer = 0
+
+                if(answer === option) correctAnswer = 1
+                return{
+                    ...state,
+                    score: state.score + correctAnswer,
+                    answerSelected: option,
+                }
 
 
         default:
